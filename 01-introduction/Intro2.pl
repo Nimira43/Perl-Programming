fe9e50604726a56c8ee444ffd6c878c684b4db24
Nimira43 <n.rabone72@aol.com>
2025-10-18 10:56:01 +0100

    More on arrays

---
+++ 01-introduction/Intro2.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+# Arrays
+my @new_array = (2, 5, 6, 7, "twelve");
+print @new_array, "\n";
+
+$new_array[2] = "Twenty";
+print @new_array, "\n";
+
+my $val01 = $new_array[2];
+print $val01, "\n";
