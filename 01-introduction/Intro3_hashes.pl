b7ab9fd962ccf1a4b3458e37137c9fd367e3f41b
Nimira43 <n.rabone72@aol.com>
2025-11-07 10:15:03 +0000

    Second example

--- 01-introduction/Intro3_hashes.pl
+++ 01-introduction/Intro3_hashes.pl
@@ -6,5 +6,6 @@ use warnings;
 my %age = ("Alice", 45, "Bob", 59, "Chloe", 23, "Dave", 32, "Emma", 38, "Frank", 63, "Gail", 54, "Harry", 55, "Isla", 73, "Jack", 18);
 
 my $val01 = $age{"Bob"};
-
 print $val01, "\n";
+my $val02 = $age{"Isla"};
+print $val02, "\n";
