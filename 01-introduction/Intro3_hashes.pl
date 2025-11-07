6be13a31e85fdce317e3da382fe8e94d64462d92
Nimira43 <n.rabone72@aol.com>
2025-11-07 10:14:05 +0000

    Getting the value of the key Bob

--- 01-introduction/Intro3_hashes.pl
+++ 01-introduction/Intro3_hashes.pl
@@ -5,4 +5,6 @@ use warnings;
 
 my %age = ("Alice", 45, "Bob", 59, "Chloe", 23, "Dave", 32, "Emma", 38, "Frank", 63, "Gail", 54, "Harry", 55, "Isla", 73, "Jack", 18);
 
-print %age, "\n";
+my $val01 = $age{"Bob"};
+
+print $val01, "\n";
