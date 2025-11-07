9a69ee23072aee9b02b5d0745d1e9fa5ce0ef00e
Nimira43 <n.rabone72@aol.com>
2025-11-07 10:10:57 +0000

    age

--- 01-introduction/Intro3_hashes.pl
+++ 01-introduction/Intro3_hashes.pl
@@ -2,3 +2,7 @@
 
 use strict;
 use warnings;
+
+my %age = ("Alice", 45, "Bob", 59, "Chloe", 23, "Dave", 32, "Emma", 38, "Frank", 63, "Gail", 54, "Harry", 55, "Isla", 73, "Jack", 18);
+
+print %age, "\n";
